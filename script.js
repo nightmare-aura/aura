fbc7174b361d108dd216b991a1efa04592954726
Aura <177610428+nightmare-aura@users.noreply.github.com>
2024-10-30 00:36:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Aura
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Aura client for Vanis.io
+// @author       Nightmare
+// @match        https://vanis.io/*
+// ==/UserScript==function showLoader() {
